153c5921909ce7af62c5d10124afacf8ff65c87b
akuaserwaad <141975577+akuaserwaad@users.noreply.github.com>
2024-09-19 22:14:00 -0400

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,19 +4,18 @@
 
 from collections import deque
 
+
 def makeChange(coins, total):
-    """Determine the fewest number of coins needed to meet a given amount total using BFS."""
+    """Function that determines the fewest number of coins needed"""
     if total <= 0:
         return 0
 
     # Initialize a queue for BFS (current sum, number of coins used)
     queue = deque([(0, 0)])  # (current_sum, num_coins_used)
     visited = set([0])  # To track the visited sums
-    
     # Start BFS
     while queue:
         current_sum, num_coins = queue.popleft()
-        
         # Explore adding each coin to the current sum
         for coin in coins:
             new_sum = current_sum + coin
@@ -25,10 +24,9 @@ def makeChange(coins, total):
             if new_sum == total:
                 return num_coins + 1
 
-            # If the new sum is valid and hasn't been visited yet, add it to the queue
+            # If we havent visited the new sum, add it to the queue
             if new_sum < total and new_sum not in visited:
                 visited.add(new_sum)
                 queue.append((new_sum, num_coins + 1))
-    
-    # If we exit the loop, it means the total cannot be met by any combination of coins
+    # If we never hit the total, return -1
     return -1
